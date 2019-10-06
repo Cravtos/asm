bb36d1f7e11364723467de1cc7477662e2688d36
cravtos <cravtos92@gmail.com>
2019-10-06 07:44:02 +0700

    Added macroses

--- rot.asm
+++ rot.asm
@@ -2,18 +2,36 @@
 ; Prints all cesar rotations of a string
 ;
 
+%define arg(n) ebp+(4*n)+4
+%define local(n) ebp-(4*n)
+
+; oneline call macro
+%macro pcall 1-*
+	%rep %0 - 1
+		%rotate -1
+			push dword %1
+	%endrep
+	%rotate -1
+		call %1
+		add esp, (%0 - 1) * 4
+%endmacro
+
+%define SYS_CALL 0x80
+%define SYS_WRITE 0x4
+%define SYS_READ 0x3
+%define SYS_EXIT 0x1
+
+%define STDOUT 0x1
+%define STDIN 0x0
+
+%define EOF 0x0
+
+%define ABC_POWER 26
+%define BUFSIZE 200
+
+
 section .data
-	endl db 0xA
-	
-	SYS_CALL equ 0x80
-	SYS_WRITE equ 0x4
-	SYS_READ equ 0x3
-	SYS_EXIT equ 0x1
-	ABC_POWER equ 26
-	STDOUT equ 0x1
-	STDIN equ 0x0
-	EOF equ 0x0
-	BUFSIZE equ 200
+	endl db 0xA  ; '\n'
 
 section .bss
 	rotated_text resb BUFSIZE
@@ -40,19 +58,32 @@ _start:
 		mov ecx, ABC_POWER
 		.rot26:
 
-			push dword [plain_text_len]
-			push ecx			; all rotations (because we're in cycle)
-			push plain_text
-			push rotated_text
-			call rot
-			add esp, 0x12
-		
-			push rotated_text
-			push plain_text_len
-			call print
-			add esp, 0x8
+			; push dword [plain_text_len]
+			; push ecx 	;(key)
+			; push plain_text
+			; push rotated_text
+			; call rot
+			; add esp, 0x12
+
+			pcall rot, rotated_text, plain_text, ecx, [plain_text_len]
+
+			; push rotated_text
+			; push plain_text_len
+			; call print
+			; add esp, 0x8
+
+			pcall print, plain_text_len, rotated_text
 
 		loop .rot26
+
+		xor eax, eax
+		mov edi, plain_text
+		mov ecx, BUFSIZE
+		rep stosb 				; Clears buffer
+		mov edi, rotated_text
+		mov ecx, BUFSIZE
+		rep stosb
+
 	loop .read_cycle
 	
 	.exit:
@@ -61,16 +92,15 @@ _start:
 		int SYS_CALL		;
 		
  
-; arg_3 -- len
-; arg_2 -- rotation
-; arg_1 -- plaintext
-; arg_0 -- place for rotated
+; arg4 -- len
+; arg3 -- rotation
+; arg2 -- plaintext
+; arg1 -- place for rotated text
 rot:
-	%define dst [ebp+8]
-	%define src [ebp+12]
-	%define rotation [ebp+16]
-	%define len [ebp+20]
-
+	%define dst arg(1)
+	%define src arg(2)
+	%define rotation arg(3)
+	%define len arg(4)
 
 	push ebp
 	mov ebp, esp
@@ -78,17 +108,19 @@ rot:
 
 	pushad
 
-	mov edi, dst
-	mov esi, src
+	mov edi, [dst]
+	mov esi, [src]
  
-	mov ecx, len
+	mov ecx, [len]
 	.cycle:
 		push ecx
 		lodsb
 
-		push eax
-		call detect_case	; EBX -> 'a' (lower) | 'A' (upper) | '0' (not a letter)
-		add esp, 0x4
+		; push eax
+		; call detect_case	; EBX -> 'a' (lower) | 'A' (upper) | '0' (not a letter)
+		; add esp, 0x4
+
+		pcall detect_case, eax
 
 		cmp ebx, '0'
 		jz .skip
@@ -96,7 +128,7 @@ rot:
 		.shift:
 			; eax = 'A' + (eax - 'A' + key) % 26
 			sub eax, ebx
-			add eax, rotation
+			add eax, [rotation]
 			xor edx, edx		;
 			mov ecx, ABC_POWER	; getting
 			div ecx				; remainder
@@ -113,6 +145,7 @@ rot:
 		%undef src
 		%undef dst
 		%undef rotation
+		%undef len
 		
 		popad
 
@@ -120,7 +153,7 @@ rot:
 		pop ebx
 		ret
 
-; arg_0 -- char
+; arg1 -- char
 ; Returns case indicator in EBX
 ; 'A' for upper
 ; 'a' for lower
@@ -129,9 +162,9 @@ detect_case:
 	push ebp
 	mov ebp, esp
 
-	%define char [ebp+8]
+	%define char arg(1)
 
-	mov eax, char
+	mov eax, [char]
 
 	cmp al, 'A'
 	jb .not_a_letter
@@ -156,14 +189,14 @@ detect_case:
 		ret
 
 
-; arg_1 -- str
-; arg_0 -- len
+; arg2 -- str
+; arg1 -- len
 print:
 	push ebp
 	mov ebp, esp
 
-	%define str [ebp+12]
-	%define len [ebp+8]
+	%define str arg(2)
+	%define len arg(1)
 
 	push eax
 	push ebx
@@ -172,8 +205,8 @@ print:
 
 	mov eax, SYS_WRITE
 	mov ebx, STDOUT
-	mov ecx, str
-	mov edx, len
+	mov ecx, [str]
+	mov edx, [len]
 	int SYS_CALL
 
 	%undef str
