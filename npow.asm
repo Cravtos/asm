7694d3b4f448a519242f0a85ee04869690578349
cravtos <cravtos92@gmail.com>
2019-10-16 01:18:43 +0700

    Unfinished npow.asm

--- npow.asm
+++ npow.asm
@@ -2,31 +2,57 @@
 ; Gets (n ** n) w/o using multiplication.
 ;
 
+%define arg(n) ebp+(4*n)+4
+%define local(n) ebp-(4*n)
+
+%define SYS_CALL 0x80
+%define SYS_WRITE 0x4
+%define SYS_READ 0x3
+%define SYS_EXIT 0x1
+
+%define STDOUT 0x1
+%define STDIN 0x0
+
+%define EOF 0x0
+%define BUFSIZE 15
+
 section .data
-    n dd 4
+    ofmsg db 'Overflow.', 10
+    ofmsglen equ ($ - ofmsg)
+
+section .bss
+    n resd 1
+    buff resb BUFSIZE
 
 section .text
     global _start
 
 _start:
+    call get_int    ; Reads int into eax
+    mov [n], eax
+
     push dword [n]  ;
     call npow       ; n ** n -> EAX
-    sub esp, 4      ;
-
+    add esp, 4      ;
     mov [n], eax    ; Result
 
-    mov eax, 1      ;
-    xor ebx, ebx    ; Terminate program
-    int 0x80        ;
+    push n
+    call print_int
+    add esp, 4
+
+    mov eax, SYS_EXIT
+    mov ebx, 0
+    int SYS_CALL
 
 
 ; arg_0 -- n
 npow:
+    %define pn arg(1)
     push ebp
     mov ebp, esp
 
     xor eax, eax
-    mov ebx, [ebp+8]
+    mov ebx, [pn]
     
     mov ecx, ebx         ; n times
     dec ecx
@@ -35,6 +61,7 @@ npow:
     dec ecx
     .sum_cycle:
         add eax, ebx
+        jo overflow
     loop .sum_cycle
 
     mov ebx, eax
@@ -42,8 +69,30 @@ npow:
     pop ecx
 loop .mult_cycle
     
+    %undef pn
     pop ebp
     ret
 
-; (n ** n) ==  (n * n) [n - 1 time] == {(n + n) [n - 1 times]} [n - 1 times] == (n + 1) [(n - 1) * (n - 1) times]
-; e.g.: 27 == 3 ** 3 == ((3 * 3) * 3) == ((3 + 3 + 3 ...
+overflow:
+    mov eax, SYS_WRITE
+    mov ebx, STDIN
+    mov ecx, ofmsg
+    mov edx, ofmsglen
+    int SYS_CALL
+
+    mov eax, SYS_EXIT
+    mov ebx, 1
+    int SYS_CALL
+
+; arg1 -- dst
+get_int:
+    %define dst arg(1)
+    push ebp
+    mov ebp, esi
+
+    mov eax, 0
+    
+
+    %undef dst
+    pop ebp
+    ret
