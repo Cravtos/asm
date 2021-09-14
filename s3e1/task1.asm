4d810ace4a3c923032344310dc3f2700bbbf5095
Nikita Kryukov <cravtos92@gmail.com>
2021-09-14 11:36:36 +0700

    Fix behaviour with negative inputs

--- s3e1/task1.asm
+++ s3e1/task1.asm
@@ -31,6 +31,9 @@ section .rodata
                 'Example ./task1 123', 0xA
     usagemsglen equ ($ - usagemsg)
 
+    new_line db 0xA
+    minus db '-'
+
 
 section .text
     global _start
@@ -39,20 +42,16 @@ _start:
     %define argc [arg(0)]
     %define argv1 [arg(2)]
     %define num [local(1)]
+    %define was_negative [local(2)]
 
     ; create stack frame
     push ebp 
     mov ebp, esp
-    sub esp, 4 ; alocate space on stack for local vars
+    sub esp, 8 ; alocate space on stack for local vars
 
     ; check if 1 argument is passed
     cmp argc, dword 2
     jne usage
-
-    ; check if passed argument is 3 digit number
-    push dword argv1
-    call check_input
-    add esp, 4
 
     ; convert string containing number to integer
     push dword argv1
@@ -60,21 +59,49 @@ _start:
     add esp, 4
     mov num, eax
 
+    cmp eax, 0
+    jl .is_negative
+
+    ;.is_positive:
+        mov was_negative, dword 0
+        jmp .endif
+
+    .is_negative:
+        mov was_negative, dword 1
+        neg eax
+        mov num, eax
+    .endif:
+
+    ; check if the passed argument is 3 digit number
+    push dword num
+    call count_digits
+    add esp, 4
+    cmp eax, 3
+    jne bad_input
+
     ; move front digit to back
     push dword num
     call front2end
     add esp, 4
     mov num, eax
 
-    ; print result
+    cmp was_negative, dword 1
+    jne .print_result
+    
+    ;.was_negative:
+        neg eax
+        mov num, eax
+
+    .print_result:
     push dword num
     call print_int
     add esp, 4
 
     ; also print new line
-    push dword 0xA
-    call putchar
-    add esp, 4
+    push dword 0x1
+    push new_line
+    call print
+    add esp, 8
 
     ; exit with code 0x0
     push 0x0
@@ -84,40 +111,39 @@ _start:
     %undef argv1
     %undef num
 
-; check_input checks if string contain 3 digit number
-; if it is not - bad input error occurs
-check_input:
+; count_digits counts amount of digits in unsigned number
+count_digits:
     %define num [arg(1)]
 
     push ebp
     mov ebp, esp
 
-    mov ecx, 0 ; digit counter
+    push ebx
+    push edx
 
-    mov esi, num
-    
+    mov eax, num
+    mov ebx, 10
+    xor ecx, ecx ; counter for digits
+
     .check_digit:
-        mov al, [esi]
-        ; al == digit?
-        cmp al, '0'
-        jb .not_digit
-        cmp al, '9'
-        ja .not_digit
+        xor edx, edx
+        test eax, eax
+        je .is_zero
 
-        inc ecx ; plus 1 digit
-        inc esi
-        jmp .check_digit
+        inc ecx
 
-    .not_digit:
-    cmp al, byte EOF
-    je .ok
-    jmp bad_input
-    .ok:
+        div ebx
+    jmp .check_digit
 
-    cmp ecx, 3
-    jne bad_input
+    .is_zero:
+    mov eax, ecx
+
+    pop edx
+    pop ebx
 
     %undef num
+
+    mov esp, ebp
     pop ebp
     ret
 
@@ -160,201 +186,175 @@ front2end:
     pop ebp
     ret
 
-; stoi takes a string containing uint/int
+; stoi takes a string containing int
 ; and returns parsed int in eax.
-; If not a number given or overflow occured,
-; it terminates a program with corresponding error.
+; If not a number is given or overflow occurred,
+; it terminates a program with a corresponding error.
 stoi:
-    %define result [local(1)]
-    %define string [arg(1)]
-
-    push ebp
-    mov ebp, esp
-    sub esp, 4
-
-    mov esi, string
-    mov ecx, 10 ; base
-    xor ebx, ebx
-
-    mov result, dword 0
-
-    ; check sign
-    mov edi, 1 ; sign (1 - positive, -1 - negative)
-    mov al, [esi]
-    inc esi
-    cmp al, '+'
-    je .loop
-    cmp al, '-'
-    jne .check_input
-    neg edi
-
-    .loop:
-        mov al, [esi]
-        inc esi
-
-        .check_input:
-
-        ; al == digit?
-        cmp al, '0'
-        jb .break
-        cmp al, '9'
-        ja .break
-
-        mov bl, al
-        
-        mov eax, result
-        mul ecx
-        jo overflow
-        
-        sub bl, '0'
-        
-        add eax, ebx
-        jo overflow
-        mov result, eax
-    jmp .loop
-    .break:
-
-    cmp al, byte EOF
-    je .ok
-    cmp al, 0xA ; '\n'
-    je .ok
-    cmp al, ' '
-    je .ok
-    jmp bad_input
-    .ok:
-
-    mov eax, result
-    cmp edi, -0x1
-    jne .positive
-    neg eax
-    .positive:
-
-
-    mov esp, ebp
-    pop ebp
-    %undef result
-    %undef string 
-    ret
-
+   %define result [local(1)]
+   %define string [arg(1)]
+ 
+   push ebp
+   mov ebp, esp
+   sub esp, 4
+ 
+   mov esi, string
+   mov ecx, 10 ; base
+   xor ebx, ebx
+ 
+   mov result, dword 0
+ 
+   ; check sign
+   mov edi, 1 ; sign (1 - positive, -1 - negative)
+   mov al, [esi]
+   inc esi
+   cmp al, '+'
+   je .loop
+   cmp al, '-'
+   jne .check_input
+   neg edi
+ 
+   .loop:
+       mov al, [esi]
+       inc esi
+ 
+       .check_input:
+ 
+       ; al == digit?
+       cmp al, '0'
+       jb .break
+       cmp al, '9'
+       ja .break
+ 
+       mov bl, al
+      
+       mov eax, result
+       mul ecx
+       jo overflow
+      
+       sub bl, '0'
+      
+       add eax, ebx
+       jo overflow
+       mov result, eax
+   jmp .loop
+   .break:
+ 
+   cmp al, byte EOF
+   je .ok
+   cmp al, 0xA ; '\n'
+   je .ok
+   cmp al, ' '
+   je .ok
+   jmp bad_input
+   .ok:
+ 
+   mov eax, result
+   cmp edi, -0x1
+   jne .positive
+   neg eax
+   .positive:
+ 
+ 
+   mov esp, ebp
+   pop ebp
+   %undef result
+   %undef string
+   ret
+ 
 ; Takes signed int and prints it
 print_int:
-    %define number [arg(1)]
-    %define buffer local(1)
-
-    push ebp
-    mov ebp, esp
-    sub esp, BUFSIZE
-
-    push eax
-    push ecx
-    push edx
-    push esi
-    push edi
-
-    mov eax, number
-    cmp eax, 0             ; is number negative or positive?
-    jnl .positive
-    neg eax
-
-    push dword '-'
-    call putchar
-    add esp, 4
-
-    .positive:
-
-    xor esi, esi           ; as len counter
-    mov ecx, 10
-    .itoc:                 ; ints to symbols
-        xor edx, edx
-        div ecx            ; n //= 10;
-        add dl, '0'
-        lea edi, [buffer+BUFSIZE-1]
-        sub edi, esi
-        mov [edi], dl
-
-        inc esi            ; ++len
-
-        cmp eax, 0
-        jle .break
-    jmp .itoc
-    .break:
-
-    lea edi, [buffer+BUFSIZE]
-    sub edi, esi
-    
-    push esi
-    push edi
-    call print
-    add esp, 8
-
-    pop edi
-    pop esi
-    pop edx
-    pop ecx
-    pop eax
-
-    %undef number
-    mov esp, ebp
-    pop ebp
-    ret
-
+   %define number [arg(1)]
+   %define buffer local(1)
+ 
+   push ebp
+   mov ebp, esp
+   sub esp, BUFSIZE
+ 
+   push eax
+   push ecx
+   push edx
+   push esi
+   push edi
+ 
+   mov eax, number
+   cmp eax, 0             ; is number negative or positive?
+   jnl .positive
+   neg eax
+ 
+   push dword 0x1
+   push minus
+   call print
+   add esp, 8
+ 
+   .positive:
+ 
+   xor esi, esi           ; as len counter
+   mov ecx, 10
+   .itoc:                 ; ints to symbols
+       xor edx, edx
+       div ecx            ; n //= 10;
+       add dl, '0'
+       lea edi, [buffer+BUFSIZE-1]
+       sub edi, esi
+       mov [edi], dl
+ 
+       inc esi            ; ++len
+ 
+       cmp eax, 0
+       jle .break
+   jmp .itoc
+   .break:
+ 
+   lea edi, [buffer+BUFSIZE]
+   sub edi, esi
+  
+   push esi
+   push edi
+   call print
+   add esp, 8
+ 
+   pop edi
+   pop esi
+   pop edx
+   pop ecx
+   pop eax
+ 
+   %undef number
+   mov esp, ebp
+   pop ebp
+   ret
+ 
 ; print takes length and string and prints string.
 ; arg2 -- str
 ; arg1 -- len
 print:
-	%define str [arg(1)]
-	%define len [arg(2)]
-	push ebp
-	mov ebp, esp
-
-    push eax
-    push ebx
-    push ecx
-    push edx
-
-	mov eax, SYS_WRITE
-	mov ebx, STDOUT
-	mov ecx, str
-	mov edx, len
-	int SYS_CALL
-
-	pop edx
-    pop ecx
-    pop ebx
-    pop eax
-
-	%undef str
-	%undef len
-
-	pop ebp
-	ret
-
-; putchar prints one char
-; arg1 -- char
-putchar:
-    %define char [arg(1)]
-	push ebp
-	mov ebp, esp
-
-    push eax
-    push ebx
-    push ecx
-    push edx
-
-	mov eax, SYS_WRITE
-	mov ebx, STDOUT
-	lea ecx, char
-	mov edx, 0x1
-	int SYS_CALL
-
-	pop edx
-    pop ecx
-    pop ebx
-    pop eax
-
-	pop ebp
-	%undef char
-	ret
+   %define str [arg(1)]
+   %define len [arg(2)]
+   push ebp
+   mov ebp, esp
+ 
+   push eax
+   push ebx
+   push ecx
+   push edx
+ 
+   mov eax, SYS_WRITE
+   mov ebx, STDOUT
+   mov ecx, str
+   mov edx, len
+   int SYS_CALL
+ 
+   pop edx
+   pop ecx
+   pop ebx
+   pop eax
+ 
+   %undef str
+   %undef len
+ 
+   pop ebp
+   ret
 
 ; == 
 ; error handlers
---
+++ s3e1/task2.asm
@@ -0,0 +1,394 @@
+; task 2: 
+; Верно ли высказывание, что цифра 5 входит в десятичную запись трехзначного целого числа k
+
+%define arg(n) ebp+(4*n)+4
+%define local(n) ebp-(4*n)
+ 
+%define SYS_CALL 0x80
+%define SYS_WRITE 0x4
+%define SYS_READ 0x3
+%define SYS_EXIT 0x1
+ 
+%define STDOUT 0x1
+%define STDIN 0x0
+ 
+%define EOF 0x0
+%define BUFSIZE 32
+ 
+section .rodata
+   msg_of db 'Overflow', 0x0A
+   msg_of_len equ ($ - msg_of)
+ 
+   msg_bi db 'Bad input', 0x0A
+   msg_bi_len equ ($ - msg_bi)
+  
+   usage_msg db 'Usage: ./task1 3-digit-number', 0x0A, \
+               'Example ./task1 153', 0x0A
+   usage_msg_len equ ($ - usage_msg)
+ 
+   contain_msg db 'Given number contains digit 5', 0x0A
+   contain_msg_len equ ($ - contain_msg)
+ 
+   not_contain_msg db 'Given number does not contain digit 5', 0x0A
+   not_contain_msg_len equ ($ - not_contain_msg)
+
+   minus db '-'
+ 
+section .text
+   global _start
+  
+_start:
+   %define argc [arg(0)]
+   %define argv1 [arg(2)]
+   %define num [local(1)]
+ 
+   ; create a stack frame
+   push ebp
+   mov ebp, esp
+   sub esp, 4 ; allocate space on the stack for local vars
+ 
+   ; check if 1 argument is passed
+   cmp argc, dword 2
+   jne usage
+ 
+   ; convert a string containing a number to an integer
+   push dword argv1
+   call stoi
+   add esp, 4
+   mov num, eax
+
+   cmp eax, 0
+   jge .is_positive
+   ; .is_negative:
+      neg eax
+      mov num, eax
+   .is_positive:
+
+   ; check if the passed argument is 3 digit number
+   push dword num
+   call count_digits
+   add esp, 4
+   cmp eax, 3
+   jne bad_input
+ 
+   ; check if the number contains 5
+   ; if so, eax will hold 1
+   push dword num
+   call contains5
+   add esp, 4
+ 
+   ; if eax == 0
+   test eax, eax
+   je .false
+   ; .true:
+      push contain_msg_len
+      push contain_msg
+      call print
+      add esp, 8
+   jmp .endif
+   .false:
+      push not_contain_msg_len
+      push not_contain_msg
+      call print
+      add esp, 8
+   .endif:
+ 
+   ; exit with code 0x0
+   push 0x0
+   call exit
+ 
+   %undef argc
+   %undef argv1
+   %undef num
+ 
+; count_digits counts amount of digits in number
+count_digits:
+   %define num [arg(1)]
+ 
+   push ebp
+   mov ebp, esp
+ 
+   push ebx
+   push edx
+ 
+   mov eax, num
+   mov ebx, 10
+   xor ecx, ecx ; counter for digits
+ 
+   .check_digit:
+      xor edx, edx
+      test eax, eax
+      je .is_zero
+
+      inc ecx
+   
+      div ebx
+   jmp .check_digit
+ 
+   .is_zero:
+   mov eax, ecx
+ 
+   pop edx
+   pop ebx
+ 
+   %undef num
+ 
+   mov esp, ebp
+   pop ebp
+   ret
+ 
+ 
+; contains5 checks if number contains digit 5
+contains5:
+   %define num [arg(1)]
+ 
+   push ebp
+   mov ebp, esp
+ 
+   push ebx
+   push edx
+ 
+   mov eax, num
+   mov ebx, 10
+ 
+   .check_digit:
+      xor edx, edx
+      test eax, eax
+      je .hasnt_5
+   
+      div ebx
+      cmp edx, 5
+      je .has_5
+   jmp .check_digit
+ 
+   .hasnt_5:
+      mov eax, 0
+   jmp .endif
+   .has_5:
+      mov eax, 1
+   .endif:
+ 
+   pop edx
+   pop ebx
+ 
+   %undef num
+ 
+   mov esp, ebp
+   pop ebp
+   ret
+ 
+; stoi takes a string containing uint/int
+; and returns parsed int in eax.
+; If not a number is given or overflow occurred,
+; it terminates a program with a corresponding error.
+stoi:
+   %define result [local(1)]
+   %define string [arg(1)]
+ 
+   push ebp
+   mov ebp, esp
+   sub esp, 4
+ 
+   mov esi, string
+   mov ecx, 10 ; base
+   xor ebx, ebx
+ 
+   mov result, dword 0
+ 
+   ; check sign
+   mov edi, 1 ; sign (1 - positive, -1 - negative)
+   mov al, [esi]
+   inc esi
+   cmp al, '+'
+   je .loop
+   cmp al, '-'
+   jne .check_input
+   neg edi
+ 
+   .loop:
+       mov al, [esi]
+       inc esi
+ 
+       .check_input:
+ 
+       ; al == digit?
+       cmp al, '0'
+       jb .break
+       cmp al, '9'
+       ja .break
+ 
+       mov bl, al
+      
+       mov eax, result
+       mul ecx
+       jo overflow
+      
+       sub bl, '0'
+      
+       add eax, ebx
+       jo overflow
+       mov result, eax
+   jmp .loop
+   .break:
+ 
+   cmp al, byte EOF
+   je .ok
+   cmp al, 0xA ; '\n'
+   je .ok
+   cmp al, ' '
+   je .ok
+   jmp bad_input
+   .ok:
+ 
+   mov eax, result
+   cmp edi, -0x1
+   jne .positive
+   neg eax
+   .positive:
+
+   mov esp, ebp
+   pop ebp
+
+   %undef result
+   %undef string
+   ret
+ 
+; Takes signed int and prints it
+print_int:
+   %define number [arg(1)]
+   %define buffer local(1)
+ 
+   push ebp
+   mov ebp, esp
+   sub esp, BUFSIZE
+ 
+   push eax
+   push ecx
+   push edx
+   push esi
+   push edi
+ 
+   mov eax, number
+   cmp eax, 0             ; is number negative or positive?
+   jnl .positive
+   neg eax
+ 
+   push dword 0x1
+   push minus
+   call print
+   add esp, 8
+ 
+   .positive:
+ 
+   xor esi, esi           ; as len counter
+   mov ecx, 10
+   .itoc:                 ; ints to symbols
+      xor edx, edx
+      div ecx            ; n //= 10;
+      add dl, '0'
+      lea edi, [buffer+BUFSIZE-1]
+      sub edi, esi
+      mov [edi], dl
+
+      inc esi            ; ++len
+
+      cmp eax, 0
+      jle .break
+   jmp .itoc
+   .break:
+ 
+   lea edi, [buffer+BUFSIZE]
+   sub edi, esi
+  
+   push esi
+   push edi
+   call print
+   add esp, 8
+ 
+   pop edi
+   pop esi
+   pop edx
+   pop ecx
+   pop eax
+ 
+   %undef number
+   mov esp, ebp
+   pop ebp
+   ret
+ 
+; print takes length and string and prints string.
+; arg2 -- str
+; arg1 -- len
+print:
+   %define str [arg(1)]
+   %define len [arg(2)]
+   push ebp
+   mov ebp, esp
+ 
+   push eax
+   push ebx
+   push ecx
+   push edx
+ 
+   mov eax, SYS_WRITE
+   mov ebx, STDOUT
+   mov ecx, str
+   mov edx, len
+   int SYS_CALL
+ 
+   pop edx
+   pop ecx
+   pop ebx
+   pop eax
+ 
+   %undef str
+   %undef len
+ 
+   pop ebp
+   ret
+ 
+; ==
+; error handlers
+; ==
+ 
+overflow:
+   push msg_of_len
+   push msg_of
+   call print
+   add esp, 8
+ 
+   push 0x1        ; overflow exitcode
+   call exit
+ 
+usage:
+   push usage_msg_len
+   push usage_msg
+   call print
+   add esp, 8
+ 
+   push 0x2        ; badinput exitcode
+   call exit
+ 
+bad_input:
+   push msg_bi_len
+   push msg_bi
+   call print
+   add esp, 8
+ 
+   push 0x2        ; badinput exitcode
+   call exit
+ 
+; exit terminates program with given exitcode
+; arg1 -- exitcode
+exit:
+   %define exitcode [arg(1)]
+   push ebp
+   mov ebp, esp
+ 
+   mov eax, SYS_EXIT
+   mov ebx, exitcode
+   int SYS_CALL
+ 
+   %undef exitcode
+   pop ebp
+   ret
